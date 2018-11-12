0a57b194aa88be773fccc7dfefed22bf1179f10b
Irene <irenehjbang@gmail.com>
2018-11-11 17:47:04 -0800

    css and js updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,136 @@
-<script>
-    /* array of words */
 
-    var words = [
-        "breaking bad",
-        "the sopranos",
-        "narcos",
-        "black mirror",
-        "homeland",
-        "sherlock",
-        "rectify",
-    ];
+//VARIABLES
+var words = [
+    "breakingbad", "thesopranos", "narcos", "blackmirror", "homeland", "sherlock", "rectify",
+    ]
 
-        /* random word picked */
-    var word = words[Math.floor(Math.random() * words.length)];
+//Empty variables to store values later
+var chosenWord = "";
+var chosenLetters = []
+var blanks = 0;
+var blanksAndCorrect = [];
+var wrongGuess = [];
 
-    /* answer array */
-    var answerArray = [];
-    for (var i = 0; i < word.length; i++) {
-        answerArray[i] = "_";
+//Counter Variables
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 9;
+
+
+
+// ALL FUNCTIONS
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+//__________________________________________________________
+//GAME START FUNCTION
+//__________________________________________________________
+function Game() {
+    chosenWord = words[Math.floor(Math.random() * words.length)];
+    chosenLetters = chosenWord.split("");
+    blanks = chosenLetters.length;
+
+    //creating a loop to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
     }
 
-    var remainingWords = word.length;
+    //showing the "_" within HTML
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
 
-    /* game loop */
-    while (remainingWords > 0) {
-        alert(answerArray.join(""));
-        var guess = prompt("Guess a letter");
-        if (guess === null) {
-            break;
+    //console logging 
+    console.log(chosenWord);
+    console.log(chosenLetters)
+    console.log(blanks)
+    console.log(blanksAndCorrect)
+}
+
+
+//__________________________________________________________
+//RESET FUNCTION
+//__________________________________________________________
+function reset() {
+    guessesRemaining = 9;
+    wrongGuess = [];
+    blanksAndCorrect = [];
+    Game()
+}
+
+//__________________________________________________________
+//CHECK LETTERS/COMPARE FUNCTION
+//__________________________________________________________
+
+//If/Else, to see if letter selected matches random word
+function checkLetters(letter) {
+    var letterInWord = false;
+    for (var i = 0; i < blanks; i++) {
+        if (randomWord[i] == letter) {
+            letterInWord = true;
         }
-        else if (guess.length !===1) {
-            ("Please enter a letter");
-        }
-        else {
-            for (var j = 0; j < word.length; j++) {
-                if (word[j) === guess);
-                    remainingWords--;
-                }
+    }
+    //if letterInWord (false)
+    if (letterInWord) {
+        for (var i = 0; i < blanks; i++) {
+            if (randomWord[i] == letter) {
+                blanksAndCorrect[i] = letter;
             }
         }
     }
+    
+    else {
+        wrongGuess.push(letter);
+        guessesRemaining--;
+    }
+    console.log(blanksAndCorrect);
+}
 
-    // show answer
+//__________________________________________________________
+//FINAL COMPLETE FUNCTION
+//__________________________________________________________
 
-    alert(answerArray.join(""));
-    alert("Contratulations! The answer is " + word);
-</script>
+//check to see if player won...
+function complete() {
+    console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
+
+    //if WON...then alert, play audio, display image and reset new round
+    if (chosenLetters.toString() == blanksAndCorrect.toString()) {
+        wins++;
+        aud()
+        reset()
+        //display wins on screen
+        document.getElementById("winstracker").innerHTML = " " + wins;
+
+        //if LOST...then alert and reset new round
+    } else if (guessesRemaining === 0) {
+        losses++;
+        reset()
+        document.getElementById("image").src = "./assets/images/try-again.png"
+        document.getElementById("losstracker").innerHTML = " " + losses;
+    }
+    //display losses on screen && guesses remaining countdown
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join(" ");
+    document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
+}
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//_____________________________________________________
+// EXECUTE CODE 
+//_____________________________________________________
+
+//call start game function
+Game()
+
+//check for keyup, and convert to lowercase then store in guesses
+document.onkeyup = function (event) {
+    var guesses = String.fromCharCode(event.keyCode).toLowerCase();
+    //check to see if guess entered matches value of random word
+    checkLetters(guesses);
+    //process wins/loss 
+    complete();
+    //store player guess in console for reference 
+    console.log(guesses);
+
+    //display/store incorrect letters on screen
+    document.getElementById("playerguesses").innerHTML = "  " + wrongGuess.join(" ");
+}
